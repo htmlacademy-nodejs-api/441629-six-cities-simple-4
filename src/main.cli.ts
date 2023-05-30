9ac30540d75e238a163456004811d8cd83ed22c0
damir <damir@gismeteo.ru>
2023-05-30 13:30:27 +0300

    Архитектура приложения

--- src/main.cli.ts
+++ src/main.cli.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import CLIApplication from './app/cli.js';
-import GenerateCommand from './core/cli-command/generate-command..js';
+import GenerateCommand from './core/cli-command/generate-command.js';
 import HelpCommand from './core/cli-command/helper.command.js';
 import ImportCommand from './core/cli-command/import.command.js';
 import VersionCommand from './core/cli-command/version.command.js';
